ea52d3564182d21c95df21160405f4ee8b3d6ced
Divyum <divyum.bhumra@xenondigilabs.com>
2020-01-18 18:09:02 +0530

    algo 1,2 and 3 solved

--- add/add.ts
+++ add/add.ts
@@ -1,13 +1,23 @@
 function add(param1: number, param2: number): number {
-
-}
-
-// function add2(param1: number[]): number {
-
-// }
-// console.log(add(1, 2));
-// console.log(add(3, 2));
-
-// console.log(add2(1,2,3,4,5));
-// console.log(add2(2,3));
-
+    const sum = param1 + param2;
+    return sum
+   }
+   
+   function add2(...param1: number[]): number {
+       let sum2 = 0;
+       
+       console.log(param1)
+   
+       param1.forEach((num) => {
+          sum2 += num
+       })
+       return sum2
+   }
+   
+   console.log(add(1, 2));
+   console.log(add(3, 2));
+   
+   console.log(add2(1,2,3,4,5));
+   console.log(add2(2,3));
+   
+   
--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,20 @@
-function addBorder(picture: string[]): string[] {
+function addBorder(...picture: string[]): string[] {
 
-}
-
-// console.log(addBorder(["abc", "ded"]));
+    const l = picture[0].length + 2
+    var line = ""
+    
+    for(var i=0;i<l;i++){
+    line += "*"
+    }
+    
+    picture[0].unshift(line)
+    picture[0].push(line)
+    
+    picture[0][1] = "*" + picture[0][1] + "*"
+    picture[0][2] = "*" + picture[0][2] + "*"
+    
+    return picture
+    
+    }
+    
+    console.log(addBorder(["abc", "ded"]));
