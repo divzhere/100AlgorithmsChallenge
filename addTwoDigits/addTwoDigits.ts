0cdf5a2e0b5670c88b13a5a6fdb7e7eb8bb76a38
Divyum <divz7777@gmail.com>
2020-04-04 08:02:22 +0530

    add two digits solved

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,8 @@
 function addTwoDigits(n: any): number {
-
+let a =  n.toString().split("")
+let a1 = parseInt(a[0])
+let a2= parseInt(a[1])
+return a1 + a2 
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
